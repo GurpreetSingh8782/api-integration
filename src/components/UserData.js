338f31864833041c9a3f15eb621fbbb579bf8262
GurpreetSingh <singhsardar8782@gmail.com>
2023-08-08 20:20:01 +0530

    api integration

---
+++ src/components/UserData.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const UserData = ({ users }) => {
+    return (
+        <>
+            {
+                users.map((curUser) => {
+                    const { id, name, email, address } = curUser;
+                    return (
+                        <tr>
+                            <td>{id}</td>
+                            <td>{name}</td>
+                            <td>{email}</td>
+                            <td>{address.city}</td>
+                        </tr>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export default UserData
